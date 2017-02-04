504b773a8f0ac4e0d9ad90342a0346cdf1fb5fe9
vincenzoarcidiacono <vinci1it2000@gmail.com>
2017-02-04 01:44:21 +0100

    fix(token): Parse Number as 0.

--- formulas/tokens/operand.py
+++ formulas/tokens/operand.py
@@ -58,8 +58,8 @@ class Error(Operand):
 
 class Number(Operand):
     _re = regex.compile(
-        '^\s*(?P<name>[1-9]+(?>\.[0-9]+)?'
-        '(?>[eE][\+\-]?[1-9]+|%)?|TRUE|FALSE)\s*',
+        '^\s*(?P<name>[0-9]+(?>\.[0-9]+)?'
+        '(?>[eE][\+\-]?[0-9]+|%)?|TRUE|FALSE)\s*',
         regex.IGNORECASE
     )
 
