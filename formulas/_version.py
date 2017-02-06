bd9c69ad66758e2fe332e506f43a7def7389a981
arcidvi <vinci1it2000@gmail.com>
2017-02-06 18:25:15 +0100

    chore(ver): bump  0.0.1

--- formulas/_version.py
+++ formulas/_version.py
@@ -8,8 +8,8 @@
 
 
 #: Authoritative project's PEP 440 version.
-__version__ = version = "0.1.0"  # Also update README.rst
+__version__ = version = "0.0.1"  # Also update README.rst
 
 # Please UPDATE TIMESTAMP WHEN BUMPING VERSIONS AND BEFORE RELEASE.
 #: Release date.
-__updated__ = "2017-01-28 02:00:00"
+__updated__ = "2017-02-06 19:00:00"
