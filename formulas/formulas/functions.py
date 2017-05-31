2ff790de2ea4ccd406a8936ebce2c79f6c873af0
Santiago Balestrini-Robinson <santiago.balestrini@gtri.gatech.edu>
2017-05-31 19:46:46 -0400

    cleaned up the ufunc wrapper and added min and max to the functions list

--- formulas/formulas/functions.py
+++ formulas/formulas/functions.py
@@ -42,6 +42,14 @@ def xsum(*args):
     return sum(list(flatten(args)))
 
 
+def xmax(*args):
+    return max([arg for arg in flatten(args) if is_number(arg)])
+
+
+def xmin(*args):
+    return min([arg for arg in flatten(args) if is_number(arg)])
+
+
 def average(*args):
     l = list(flatten(args))
     return sum(l) / len(l)
@@ -80,34 +88,34 @@ def raise_errors(*args):
             raise FoundError(err=v)
 
 
-def call_ufunc(value, ufunc, xl_error):
+def call_ufunc(value, ufunc):
+    """Helps call a numpy universal function (ufunc)."""
+
     def safe_eval(f, val):
         try:
             res = f(val)
             return res if all((res < inf, res > -inf)) else np.nan
         except (ValueError, TypeError):
-            return np.nan
+            return Error.errors['#VALUE!']
 
     if isinstance(value, (list, tuple, np.ndarray)):
         # Make an array for the error
-        err_arr = np.where(np.ones(value.shape), xl_error, None)
+        err_arr = np.where(np.ones(value.shape), Error.errors['#NUM!'], None)
         result = np.reshape([safe_eval(ufunc, item) for item in np.ravel(value)],
                             value.shape)
         # Replace `nan` with appropriate error
         return np.where(np.isnan(result), err_arr, result)
     else:
         result = safe_eval(ufunc, value)
-        return xl_error if result is np.nan else result
+        if result is np.nan:
+            return Error.errors['#NUM!']
+        else:
+            return result
 
 
-def wrap_func(func, xl_error='#VALUE!'):
+def wrap_func(func):
     if func in ufuncs:
-        func = functools.partial(call_ufunc,
-                                 ufunc=ufuncs[func],
-                                 xl_error=xl_error)
-
-    if isinstance(xl_error, str):
-        xl_error = Error.errors[xl_error]
+        func = functools.partial(call_ufunc, ufunc=ufuncs[func])
 
     def wrapper(*args, **kwargs):
         # noinspection PyBroadException
@@ -117,36 +125,39 @@ def wrap_func(func, xl_error='#VALUE!'):
         except FoundError as ex:
             return np.asarray([[ex.err]], object)
         except:
-            return np.asarray([[xl_error]], object)
+            return np.asarray([[Error.errors['#VALUE!']]], object)
     return functools.update_wrapper(wrapper, func)
 
 
 FUNCTIONS = collections.defaultdict(lambda: not_implemented)
 FUNCTIONS.update({
-    'ACOS': wrap_func('arccos', '#NUM!'),
-    'ACOSH': wrap_func('arccosh', '#NUM!'),
+    'ABS': wrap_func('abs'),
+    'ACOS': wrap_func('arccos'),
+    'ACOSH': wrap_func('arccosh'),
     'ARRAY': lambda *args: np.asarray(args, object).view(Array),
     'ARRAYROW': lambda *args: np.asarray(args, object).view(Array),
-    'ASIN': wrap_func('arcsin', '#NUM!'),
-    'ASINH': wrap_func('arcsinh', '#NUM!'),
-    'ATAN': wrap_func('arctan', '#NUM!'),
-    'ATAN2': wrap_func('arctan2', '#NUM!'),
-    'ATANH': wrap_func('arctanh', '#NUM!'),
+    'ASIN': wrap_func('arcsin'),
+    'ASINH': wrap_func('arcsinh'),
+    'ATAN': wrap_func('arctan'),
+    'ATAN2': wrap_func('arctan2'),
+    'ATANH': wrap_func('arctanh'),
     'AVERAGE': wrap_func(average),
-    'COS': wrap_func('cos', '#NUM!'),
-    'COSH': wrap_func('cosh', '#NUM!'),
-    'EXP': wrap_func('exp', '#NUM!'),
+    'COS': wrap_func('cos'),
+    'COSH': wrap_func('cosh'),
+    'EXP': wrap_func('exp'),
     'IF': wrap_func(lambda c, x=True, y=False: np.where(c, x, y)),
     'IFERROR': iferror,
     'INT': wrap_func(int),
     'ISERR': iserr,
     'ISERROR': iserror,
-    'LOG': wrap_func('log10', '#NUM!'),
-    'LN': wrap_func('log', '#NUM!'),
+    'LOG': wrap_func('log10'),
+    'LN': wrap_func('log'),
+    'MAX': wrap_func(xmax),
+    'MIN': wrap_func(xmin),
     'PI': lambda: math.pi,
-    'SIN': wrap_func('sin', '#NUM!'),
-    'SINH': wrap_func('sinh', '#NUM!'),
+    'SIN': wrap_func('sin'),
+    'SINH': wrap_func('sinh'),
     'SUM': wrap_func(xsum),
-    'TAN': wrap_func('tan', '#NUM!'),
-    'TANH': wrap_func('tanh', '#NUM!'),
+    'TAN': wrap_func('tan'),
+    'TANH': wrap_func('tanh'),
 })
