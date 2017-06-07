0d4d1a34508dbb3b3dc0c25ffed8b031c999d739
Vincenzo Arcidiacono <vinci1it2000@gmail.com>
2017-06-07 16:50:18 +0200

    fix(functions), #4: Simplify ufunc code.

--- formulas/formulas/functions.py
+++ formulas/formulas/functions.py
@@ -13,12 +13,10 @@ import functools
 import collections
 import math
 import numpy as np
-from schedula.utils import EMPTY
 from ..errors import FunctionError, FoundError
 from ..tokens.operand import XlError, Error
 
 
-inf = float('inf')
 ufuncs = {item: getattr(np, item) for item in dir(np)
           if isinstance(getattr(np, item), np.ufunc)}
 
@@ -89,43 +87,29 @@ def raise_errors(*args):
             raise FoundError(err=v)
 
 
-def call_ufunc(*args, ufunc=''):
+def call_ufunc(ufunc, *args):
     """Helps call a numpy universal function (ufunc)."""
-
-    def safe_eval(f, *vals):
-        vals = [0. if val is EMPTY else val for val in vals]
+    from .operators import _replace_empty
+    def safe_eval(*vals):
         try:
-            res = f(*vals)
-            return res if all((res < inf, res > -inf)) else np.nan
+            r = ufunc(*map(float, vals))
+            if not isinstance(r, XlError) and (np.isnan(r) or np.isinf(r)):
+                r = Error.errors['#NUM!']
         except (ValueError, TypeError):
-            return Error.errors['#VALUE!']
+            r = Error.errors['#VALUE!']
+        return r
+    # Check all arrays are the same length
+    # Excel returns #VAlUE! error if they don't match
+    # e.g., SUMPRODUCT
+    assert len(set(len(arg) for arg in args)) == 1
 
-    if isinstance(args[0], (list, tuple, np.ndarray)):
-        # Check all arrays are the same length
-        # Excel returns #VAlUE! error if they don't match
-        # e.g., SUMPRODUCT
-        if len(set(len(arg) for arg in args)) != 1:
-            print('len', args)
-            return Error.errors['#VALUE!']
-
-        # Make an array for the error
-        err_arr = np.where(np.ones(args[0].shape), Error.errors['#NUM!'], None)
-        result = np.reshape([safe_eval(ufunc, *inputs)
-                             for inputs in zip(*(np.ravel(arg) for arg in args))],
-                            args[0].shape)
-        # Replace `nan` with appropriate error
-        return np.where(np.isnan(result), err_arr, result)
-    else:
-        result = safe_eval(ufunc, *args)
-        if result is np.nan:
-            return Error.errors['#NUM!']
-        else:
-            return result
+    res = np.vectorize(safe_eval, otypes=[object])(*map(_replace_empty, args))
+    return res.view(Array)
 
 
 def wrap_func(func, args_indices=None):
     if func in ufuncs:
-        func = functools.partial(call_ufunc, ufunc=ufuncs[func])
+        func = functools.partial(call_ufunc, ufuncs[func])
 
     def wrapper(*args, **kwargs):
         # noinspection PyBroadException
