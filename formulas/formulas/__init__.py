fix(functions.logic): Correct error behaviour of `if` and `iferror` functions + add BroadcastError.

formulas/formulas/__init__.py
@@ -23,7 +23,7 @@ Sub-Modules:
 import functools
 import numpy as np
 import schedula as sh
-from ..errors import RangeValueError, FunctionError, FoundError
+from ..errors import RangeValueError, FunctionError, FoundError, BaseError
 from ..tokens.operand import Error
 
 
@@ -69,6 +69,8 @@ def wrap_func(func):
             return func(*args, **kwargs)
         except FoundError as ex:
             return np.asarray([[ex.err]], object)
+        except BaseError as ex:
+            raise ex
         except:
             return np.asarray([[Error.errors['#VALUE!']]], object)
 

formulas/formulas/functions/__init__.py
@@ -27,7 +27,7 @@ import importlib
 import collections
 import numpy as np
 from .. import replace_empty, not_implemented, Array, wrap_func
-from ...errors import FoundError
+from ...errors import FoundError, BroadcastError
 from ...tokens.operand import XlError, Error
 
 SUBMODULES = ['.info', '.logic', '.math', '.stat']
@@ -70,7 +70,7 @@ def flatten(l, check=is_number):
 
 def wrap_ufunc(
         func, input_parser=lambda *a: map(float, a), check_error=get_error,
-        args_parser=lambda *a: map(replace_empty, a)):
+        args_parser=lambda *a: map(replace_empty, a), otype=lambda *a: Array):
     """Helps call a numpy universal function (ufunc)."""
 
     def safe_eval(*vals):
@@ -85,9 +85,16 @@ def wrap_ufunc(
 
     # noinspection PyUnusedLocal
     def wrapper(*args, **kwargs):
-        args = args_parser(*args)
-        return np.vectorize(safe_eval, otypes=[object])(*args).view(Array)
-
+        try:
+            args = tuple(args_parser(*args))
+            func = np.vectorize(safe_eval, otypes=[object])
+            return func(*args).view(otype(*args))
+        except ValueError as ex:
+            try:
+                np.broadcast(*args)
+            except ValueError:
+                raise BroadcastError()
+            raise ex
     return wrap_func(functools.update_wrapper(wrapper, func))
 
 

formulas/formulas/functions/logic.py
@@ -9,16 +9,47 @@
 """
 Python equivalents of logical excel functions.
 """
-import numpy as np
-from . import wrap_func
+from . import wrap_ufunc, Array, Error, flatten, get_error
 
 FUNCTIONS = {}
-FUNCTIONS['IF'] = wrap_func(lambda c, x=True, y=False: np.where(c, x, y))
 
 
-def iferror(val, val_if_error):
+class IfArray(Array):
+    def collapse(self, shape):
+        if tuple(shape) == (1, 1) != self.shape:
+            return Error.errors['#VALUE!']
+        return super(IfArray, self).collapse(shape)
+
+
+def xif(condition, x=True, y=False):
+    return x if condition else y
+
+
+FUNCTIONS['IF'] = wrap_ufunc(xif, otype=lambda *a: IfArray)
+
+
+class IfErrorArray(Array):
+    _value = Error.errors['#VALUE!']
+
+    def collapse(self, shape):
+        if tuple(shape) == (1, 1) != self.shape:
+            return self._value
+        return super(IfErrorArray, self).collapse(shape)
+
+
+def xiferror(val, val_if_error):
     from .info import iserror
-    return np.where(iserror(val), val_if_error, val)
+    return val_if_error if iserror(val) else val
 
 
-FUNCTIONS['IFERROR'] = iferror
+def xiferror_otype(val, val_if_error):
+    class _IfErrorArray(IfErrorArray):
+        _value = list(flatten(val_if_error, None))[0]
+
+    return _IfErrorArray
+
+
+FUNCTIONS['IFERROR'] = wrap_ufunc(
+    xiferror, input_parser=lambda *a: a,
+    check_error=lambda *a: False, otype=xiferror_otype
+)