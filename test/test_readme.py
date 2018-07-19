c4199829878ae68f994d43a2dc4df480dbbff383
vincenzoarcidiacono <vinci1it2000@gmail.com>
2018-07-19 19:15:37 +0200

    test(readme): Add test of readme.

---
+++ test/test_readme.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+#
+# Copyright 2014-2017 European Commission (JRC);
+# Licensed under the EUPL (the 'Licence');
+# You may not use this work except in compliance with the Licence.
+# You may obtain a copy of the Licence at: http://ec.europa.eu/idabc/eupl
+import os
+import doctest
+import unittest
+
+
+EXTRAS = os.environ.get('EXTRAS', 'all')
+
+
+@unittest.skipIf(EXTRAS not in ('all',), 'Not for extra %s.' % EXTRAS)
+class TestDoctest(unittest.TestCase):
+    def runTest(self):
+        failure_count, test_count = doctest.testfile(
+            '../README.rst', optionflags=doctest.NORMALIZE_WHITESPACE | doctest.ELLIPSIS
+        )
+        self.assertGreater(test_count, 0, (failure_count, test_count))
+        self.assertEqual(failure_count, 0, (failure_count, test_count))
