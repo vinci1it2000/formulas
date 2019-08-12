037f3856674f067e86d44258bfa96b171119a8cc
vincenzoarcidiacono <vinci1it2000@gmail.com>
2019-08-12 20:36:17 +0200

    feat(stat), #35: Add `MINA`, `AVERAGEA`, `MAXA` functions.

--- formulas/functions/stat.py
+++ formulas/functions/stat.py
@@ -9,33 +9,48 @@
 """
 Python equivalents of statistical Excel functions.
 """
-from . import raise_errors, flatten, wrap_func, Error
+import functools
+import schedula as sh
+from . import raise_errors, flatten, wrap_func, Error, is_number
 
 FUNCTIONS = {}
 
 
-def xaverage(*args):
+def is_not_empty(v):
+    return v is not sh.EMPTY
+
+
+def _convert(v):
+    if isinstance(v, str):
+        return 0
+    if isinstance(v, bool):
+        return int(v)
+    return v
+
+
+def xfunc(*args, func=max, check=is_number, convert=None, default=0):
     raise_errors(args)
-    v = list(flatten(args))
-    if v:
+    it = flatten(args, check=check)
+    return func(list(map(convert, it) if convert else it) or [default])
+
+
+def xaverage(v):
+    if v[0] is not None:
         return sum(v) / len(v)
     return Error.errors['#DIV/0!']
 
 
-FUNCTIONS['AVERAGE'] = wrap_func(xaverage)
-
-
-def xmax(*args):
-    raise_errors(args)
-    return max(list(flatten(args)) or [0])
-
-
-FUNCTIONS['MAX'] = wrap_func(xmax)
-
-
-def xmin(*args):
-    raise_errors(args)
-    return min(list(flatten(args)) or [0])
-
-
-FUNCTIONS['MIN'] = wrap_func(xmin)
+FUNCTIONS['AVERAGE'] = wrap_func(functools.partial(
+    xfunc, func=xaverage, default=None
+))
+FUNCTIONS['AVERAGEA'] = wrap_func(functools.partial(
+    xfunc, convert=_convert, check=is_not_empty, func=xaverage, default=None
+))
+FUNCTIONS['MAX'] = wrap_func(xfunc)
+FUNCTIONS['MAXA'] = wrap_func(functools.partial(
+    xfunc, convert=_convert, check=is_not_empty
+))
+FUNCTIONS['MIN'] = wrap_func(functools.partial(xfunc, func=min))
+FUNCTIONS['MINA'] = wrap_func(functools.partial(
+    xfunc, convert=_convert, check=is_not_empty, func=min
+))
