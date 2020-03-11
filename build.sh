cf59e884cc65def60ae66c50fd1e9b37373e4599
vincenzoarcidiacono <vinci1it2000@gmail.com>
2020-03-11 08:09:41 +0100

    feat(core): Update build script.

--- build.sh
+++ build.sh
@@ -5,12 +5,25 @@ export ENABLE_SETUP_LONG_DESCRIPTION="TRUE"
 python setup.py sdist bdist_wheel -v
 
 while true; do
-    echo "Do you wish to publish the package on PyPI?[Y/n]"
+    echo "Do you wish to publish the package?[Y/n]"
     read yn
     case ${yn} in
         [Yy]|"")
-        twine upload dist/*
-        exit;;
+        while true; do
+            echo "Do you wish to publish on PyPI ('n' for TestPyPI)?[Y/n]"
+            read yn
+            case ${yn} in
+                [Yy]|"")
+                twine upload dist/*
+                exit;;
+
+                [Nn])
+                twine upload --repository testpypi dist/*
+                exit;;
+
+                *) echo "Please answer y/n.";;
+            esac
+        done;;
 
         [Nn]) break;;
 
