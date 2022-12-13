f1d31ab15c9e6f16783a1fbf29e1e5600b339873
vincenzoarcidiacono <vinci1it2000@gmail.com>
2022-12-13 23:11:35 +0100

    chore(ver): bump 1.2.5 --> 1.2.6.

--- formulas/_version.py
+++ formulas/_version.py
@@ -10,11 +10,11 @@ __all__ = ['__version__', '__updated__', '__title__', '__author__',
            '__license__', '__copyright__']
 
 #: Authoritative project's PEP 440 version.
-__version__ = version = "1.2.5"  # Also update README.rst
+__version__ = version = "1.2.6"  # Also update README.rst
 
 # Please UPDATE TIMESTAMP WHEN BUMPING VERSIONS AND BEFORE RELEASE.
 #: Release date.
-__updated__ = "2022-11-07 13:45:00"
+__updated__ = "2022-12-13 23:15:00"
 
 __title__ = 'formulas'
 
